4f4f033c9baec1b58ececf1bab2db696f49e50f9
pitsanders <tomashevskiyt@gmail.com>
2024-03-05 10:17:11 -0500

    Testing new function

--- main.py
+++ main.py
@@ -10,9 +10,17 @@ def print_hi(name):
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
+
+
+def my_function(a,b):
+     return a + b
+
+# Comment
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('Hi Pegggggderco')
     print("howareU")
-
+# copy of the repository
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    print(my_function(2,4))
+
